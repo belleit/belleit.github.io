671fba623dd48f1bdfdb498f7aed268bf83cf1ba
monsieurverde <domverdemgmt@gmail.com>
2020-08-09 18:59:30 -0400

    added privacy and covid statement

---
+++ precache-manifest.7e1c2e1e0761bb7edf338aa9f04225a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5bad78fa6a59b59dac3963d2ea7eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0899d2900e3abdfaf2e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da253b6884b16b7f2b04",
+    "url": "/static/js/2.2a448456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a448456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0899d2900e3abdfaf2e",
+    "url": "/static/js/main.3551e45c.chunk.js"
+  },
+  {
+    "revision": "12c06e441e44c3dbff98",
+    "url": "/static/js/runtime-main.606f6612.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
