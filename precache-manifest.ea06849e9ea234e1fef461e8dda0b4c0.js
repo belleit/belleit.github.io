0462413f9198296a657c36f958aa9d2c14485592
monsieurverde <domverdemgmt@gmail.com>
2020-08-09 21:52:18 -0400

    cleanup

---
+++ precache-manifest.ea06849e9ea234e1fef461e8dda0b4c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d1eee8cb39d61f5ece329625a43f94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c3dba910e6fc5b2a0b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da5196caccfa4824eac5",
+    "url": "/static/js/2.b4ec1a94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4ec1a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c3dba910e6fc5b2a0b",
+    "url": "/static/js/main.463644fe.chunk.js"
+  },
+  {
+    "revision": "12c06e441e44c3dbff98",
+    "url": "/static/js/runtime-main.606f6612.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
